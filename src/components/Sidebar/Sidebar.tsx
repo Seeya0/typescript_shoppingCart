a35b0aeee288297d7b90c11f0e8a2897a090e781
Seeya0 <vinlandtorfin@gmail.com>
2022-01-27 22:55:31 +0900

    sidebarコンポーネントを追加

---
+++ src/components/Sidebar/Sidebar.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Sidebar = () => {
+    return (
+        <div className="flex flex-col justify-between bg-white h-full overflow-y-scroll hidden-scrollbar">
+            <h1>たまごたまご</h1>
+        </div>
+    )
+}
+
+export default Sidebar
